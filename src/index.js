1d1817e648bc204ff553991dc937aabbdaba4918
jsant0s3189 <jsant0s3189@gmail.com>
2018-02-17 13:28:54 -0600

    add firebase auth

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BrowserRouter } from 'react-router-dom';
+import firebase from 'firebase';
 
 import 'reset-css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const config = {
+  apiKey: 'AIzaSyDrzKn5dQGcbyW5dthYuUCs53oswggB4PQ',
+  authDomain: 'react-phones.firebaseapp.com',
+  databaseURL: 'https://react-phones.firebaseio.com',
+  projectId: 'react-phones',
+  storageBucket: '',
+  messagingSenderId: '989699814667',
+};
+firebase.initializeApp(config);
 
 /* eslint react/jsx-filename-extension: 0 */
 ReactDOM.render(
